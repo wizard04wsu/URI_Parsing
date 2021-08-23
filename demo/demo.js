3e1d0da8dbdda8e2f1fcbe769547fed849f34062
wizard04wsu <wizard04@gmail.com>
2021-08-23 01:26:57 -0400

    - rename .mjs to .js

--- demo/demo.js
+++ demo/demo.js
@@ -1,3 +1,3 @@
-import URI, {URIError, isDNSDomain, removeDotSegments} from "https://github.com/wizard04wsu/URI_Parsing/tree/dev/src/uri_parsing.mjs"
+import URI, {URIError, isDNSDomain, parseMailbox} from "https://github.com/wizard04wsu/URI_Parsing/tree/dev/src/uri_parsing.js"
 
 console.log(URI("https://www.google.com/search?q=javascript+is+cool&ie=UTF-8#footcnt"));
