2c1b94badd32197fcc4641cf1c2bbb418e5e480b
Andy Harrison <wizard04@gmail.com>
2022-03-12 02:52:40 -0500

    update jsfiddle demo

--- demo/demo.js
+++ demo/demo.js
@@ -5,11 +5,10 @@ console.clear();
 document.getElementById("parse_scheme").addEventListener("click", ()=>{
 	let val = document.getElementById("uri").value;
 	console.group("scheme");
-	console.log(val);
 	try{
-		val = URI(val);
-		console.log(val.toString());
-		console.log(val);
+		let obj = URI(val);
+		console.log("--> "+obj.toString());
+		console.log(obj);
 	}catch(e){
 		if(e instanceof URIError) console.error(e.message);
 		else throw e;
@@ -21,11 +20,10 @@ document.getElementById("parse_scheme").addEventListener("click", ()=>{
 document.getElementById("parse_generic").addEventListener("click", ()=>{
 	let val = document.getElementById("uri").value;
 	console.group("generic");
-	console.log(val);
 	try{
-		val = URI.parse(val)
-		console.log(val.toString());
-		console.log(val);
+		let obj = URI.parse(val)
+		console.log("--> "+obj.toString());
+		console.log(obj);
 	}catch(e){
 		if(e instanceof URIError) console.error(e.message);
 		else throw e;
@@ -37,11 +35,10 @@ document.getElementById("parse_generic").addEventListener("click", ()=>{
 document.getElementById("parse_mailbox").addEventListener("click", ()=>{
 	let val = document.getElementById("email").value;
 	console.group("email");
-	console.log(val);
 	try{
-		val = parseMailbox(val);
-		console.log(val.full);
-		console.log(val);
+		let obj = parseMailbox(val);
+		console.log("--> "+obj.toString());
+		console.log(obj);
 	}catch(e){
 		if(e instanceof URIError) console.log(e.message);
 		else throw e;
